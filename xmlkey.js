8e4048c7f0144039960a951f955ff98c8791902a
jungdongwook <nolhoon211@gmail.com>
2022-09-22 08:50:38 +0900

    이그노어 다시

--- xmlkey.js
+++ xmlkey.js
@@ -1,3 +1,2 @@
-const config = {
-    xmlkey : "aabdjVtDyODuXtGkvJfA7GEEAE%2B7oKgHMt3Vs2z1iZZy%2Fh0S9KF7iOxPZFyyqKg28lO9bKPRcx3WzJxQtZnlXg%3D%3D"
-}
+const EncodingKEY = `
+aabdjVtDyODuXtGkvJfA7GEEAE%2B7oKgHMt3Vs2z1iZZy%2Fh0S9KF7iOxPZFyyqKg28lO9bKPRcx3WzJxQtZnlXg%3D%3D`
